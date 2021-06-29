e1e1e412b0385b53f0c99e7ca01fd82215aeb2f4
alexeybokhovkin <bohovkinalex@gmail.com>
2021-06-29 14:36:02 +0200

    environment & config

--- dataproc/main_directories.py
+++ dataproc/main_directories.py
@@ -1,11 +1,8 @@
 
 STRUCT_DATA_DIR = '/home/alexey_bokhovkin/projects/scannet-part-segmentation/struct_data'
 REFINED_STRUCT_DATA_DIR = '/home/alexey_bokhovkin/projects/scannet-part-segmentation/data'
-PARTNET = '/home/ishvlad/datasets/PartNet'
-PARTNET_VOXELIZED = '/home/ishvlad/workspace/part-segmentation/Assets/partnet-full-voxelized-32'
-SHAPENET = '/home/ishvlad/workspace/Scan2CAD/Assets/full/shapenet'
-SHAPENET_FULL = '/home/ishvlad/datasets/ShapeNetCore.v2'
-DICTIONARIES = '/home/ishvlad/workspace/part-segmentation/Assets/dictionaries'
-
-ASSETS = '/home/ishvlad/workspace/part-segmentation/Assets/'
-PARTS_TO_SHAPES = '/home/ishvlad/workspace/part-segmentation/Assets/dictionaries/parts_to_shapes.json'
+PARTNET = '/home/alexey_bokhovkin/datasets/PartNet'
+PARTNET_VOXELIZED = '/home/alexey_bokhovkin/workspace/part-segmentation/Assets/partnet-full-voxelized-32'
+SHAPENET = '/home/alexey_bokhovkin/workspace/Scan2CAD/Assets/full/shapenet'
+SHAPENET_FULL = '/home/alexey_bokhovkin/datasets/ShapeNetCore.v2'
+DICTIONARIES = '/home/alexey_bokhovkin/workspace/part-segmentation/Assets/dictionaries'
